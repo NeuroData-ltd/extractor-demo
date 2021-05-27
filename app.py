9e070c728c6bd1cca19ad84833b1e73824301c73
NeuroData <67514861+NeuroData-ltd@users.noreply.github.com>
2021-05-27 23:37:17 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,22 +7,26 @@ import json
 import pandas as pd
 import os
 
-
 from collections import namedtuple
 import warnings
+
 analyzedDocument = namedtuple('AnalyzedDocument', 'words tags')
 
 items = ['abn_number', 'account_number', 'bill_to_address', 'bill_to_name', 'bill_to_vat_number', 'card_number',
          'cashback', 'category', 'currency_code', 'date', 'delivery_date', 'discount', 'document_ref',
-         'due_date','img_file_name', 'img_thumbnail', 'img_url', 'insurance', 'invoice_number', 'is_duplicate',
-         'description', 'order', 'quantity', 'reference', 'section', 'sku', 'tax', 'tax_rate', 'total', 'type', 'unit_of_measure',
-         'ocr_text', 'order_date', 'payment_disp', 'payment_terms', 'payment_type', 'phone_number', 'purchase_order', 'rounding',
-         'service_end_date', 'service_start_date', 'ship_date', 'ship_to_address', 'ship_to_name', 'shipping', 'store_number', 'subtotal', 'tax_lines', 'tip', 'total_weight',
-         'tracking_number','vat_number', 'address', 'email', 'fax_number', 'name', 'raw_name', 'vendor_logo', 'vendor_reg', 'vendor_type', 'web',
+         'due_date', 'img_file_name', 'img_thumbnail', 'img_url', 'insurance', 'invoice_number', 'is_duplicate',
+         'description', 'order', 'quantity', 'reference', 'section', 'sku', 'tax', 'tax_rate', 'total', 'type',
+         'unit_of_measure',
+         'ocr_text', 'order_date', 'payment_disp', 'payment_terms', 'payment_type', 'phone_number', 'purchase_order',
+         'rounding',
+         'service_end_date', 'service_start_date', 'ship_date', 'ship_to_address', 'ship_to_name', 'shipping',
+         'store_number', 'subtotal', 'tax_lines', 'tip', 'total_weight',
+         'tracking_number', 'vat_number', 'address', 'email', 'fax_number', 'name', 'raw_name', 'vendor_logo',
+         'vendor_reg', 'vendor_type', 'web',
          'vendor_account', 'vendor_bank', 'vendor_bank_number', 'vendor_bank_swift', 'vendor_iban']
-CLIENT_ID = "vrf3ZPfMXtt4vzaEtCYChFXtz4GemhUoNOXY4Xt"
+CLIENT_ID = "vrfncE0Py8CCX8fzP5CoClKhOvVhngBcsvncwfh"
 ENVIRONMENT_URL = "api.veryfi.com"
-username = "yassine.hamdaoui"
+username = "contact.neurodata"
 api_key = "501237936122316437457a0df27a20e9"
 process_file_url = 'https://{0}/api/v7/partner/documents/'.format(ENVIRONMENT_URL)
 headers = {
@@ -31,81 +35,49 @@ headers = {
     "AUTHORIZATION": "apikey {0}:{1}".format(username, api_key)
 }
 
-
-
 url = 'https://app.nanonets.com/api/v2/OCR/Model/54ead1ca-698f-4e41-92c2-7cbde54f7e3b/LabelFile/'
 
 st.set_page_config(
     page_title="NeuroData Extractor Demo", layout="wide", page_icon="./images/logo.png"
 )
-list_options = ['abn_number', 'account_number', 'bill_to_address', 'bill_to_name', 'bill_to_vat_number', 'card_number', 'cashback', 'category', 'created', 'currency_code', 'date', 'delivery_date', 'discount', 'document_reference_number', 'due_date', 'external_id', 'id', 'img_file_name', 'img_thumbnail_url', 'img_url', 'insurance', 'invoice_number', 'is_duplicate', 'line_items', 'ocr_text', 'order_date', 'payment_display_name', 'payment_terms', 'payment_type', 'phone_number', 'purchase_order_number', 'rounding', 'service_end_date', 'service_start_date', 'ship_date', 'ship_to_address', 'ship_to_name', 'shipping', 'store_number', 'subtotal', 'tax', 'tax_lines', 'tip', 'total', 'total_weight', 'tracking_number', 'updated', 'vat_number', 'vendor', 'vendor_account_number', 'vendor_bank_name', 'vendor_bank_number', 'vendor_bank_swift', 'vendor_iban']
+
 st.title("NeuroData Extractor Demo")
 st.subheader("")
 c = st.beta_columns(9)
 a = c[0].checkbox("select all")
 
-
-st.markdown("---")
-st.subheader("Select Items :")
-st.subheader("")
-cols= st.beta_columns(9)
-for i in range(9):
-    cols[i].checkbox(items[i],key=i,value=a)
-cols= st.beta_columns(9)
-for i in range(9,18):
-    cols[i-9].checkbox(items[i],key=i,value=a)
-cols= st.beta_columns(9)
-for i in range(28,37):
-    cols[i-28].checkbox(items[i],key=i,value=a)
-cols= st.beta_columns(9)
-for i in range(37,46):
-    cols[i-37].checkbox(items[i],key=i,value=a)
-cols= st.beta_columns(9)
-for i in range(46,55):
-    cols[i-46].checkbox(items[i],key=i,value=a)
-cols= st.beta_columns(9)
-for i in range(55,64):
-    cols[i-55].checkbox(items[i],key=i,value=a)
-cols= st.beta_columns(9)
-for i in range(64,66):
-    cols[i-64].checkbox(items[i],key=i,value=a)
 st.markdown("---")
 
 
-
-
-
-st.sidebar.image("images/logo.png",width=120)
+st.sidebar.image("images/logo.png", width=120)
 st.sidebar.title("PDF Scraper For Businesses")
 st.sidebar.image(r"images/extractor.gif")
 st.sidebar.title("Reduce your manual data entry costs")
 st.sidebar.image(r"images/ocr_illustration.gif")
 
-img_file_buffer = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg","pdf"])
-
+img_file_buffer = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg", "pdf"])
 
 tic = time.time()
 if img_file_buffer is not None:
-    file_details = {"FileName":img_file_buffer.name,"FileType":img_file_buffer.type}
+    file_details = {"FileName": img_file_buffer.name, "FileType": img_file_buffer.type}
 
     with open(os.path.join("tempDir", img_file_buffer.name), "wb") as f:
         f.write(img_file_buffer.getbuffer())
     st.success("Saved File")
     st.markdown("---")
     # Store Pdf with convert_from_path function
-    fn = "./tempDir/"+img_file_buffer.name
-    
+    fn = "./tempDir/" + img_file_buffer.name
+
     os.system('convert ' +
-                  '-density 300 ' +
-                  '%s ' % fn +
-                  '-quality 100 ' +
-                  '-flatten ' +
-                  '%s' % fn.replace('.pdf', '.jpg'))
-    print("Done")
+              '-density 300 ' +
+              '%s ' % fn +
+              '-quality 100 ' +
+              '-flatten ' +
+              '%s' % fn.replace('.pdf', '.jpg'))
+    
 
-
-    image_path = "tempDir/"+img_file_buffer.name.split(".")[0]+ '.jpg'
-    file_name = img_file_buffer.name.split(".")[0]+ '.jpg'
+    image_path = "tempDir/" + img_file_buffer.name.split(".")[0] + '.jpg'
+    file_name = img_file_buffer.name.split(".")[0] + '.jpg'
 
     if st.button("Process Your Invoices"):
         # You can send the list of categories that is relevant to your case
@@ -120,27 +92,14 @@ if img_file_buffer is not None:
 
         df = response.json()
         dd = {}
-        for key,value in df.items():
-            if key=="vendor":
-                for k,v in df["vendor"].items():
-                    dd[k]=v
-            if key =="line_items":
-                for k,v in df["line_items"][0].items():
-                    dd[k]=v
+        for key, value in df.items():
+            if key == "vendor":
+                for k, v in df["vendor"].items():
+                    dd[k] = v
+            if key == "line_items":
+                for k, v in df["line_items"][0].items():
+                    dd[k] = v
             else:
                 dd[key] = value
 
         st.json(dd)
-
-
-
-
-
-
-
-
-
-
-
-
-
